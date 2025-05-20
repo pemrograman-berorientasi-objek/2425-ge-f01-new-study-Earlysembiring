3c717eeb49890444d294b190c6a5a618f718f835
Earlysembiring <nathaliasembiring06@gmail.com>
2025-05-20 16:18:23 +0700

    pair 19

---
+++ src/main/java/pbo/model/Student.java
@@ -0,0 +1,55 @@
+package pbo.model;
+import javax.persistence.*;
+
+@Entity
+public class Student {
+    @Id
+    @Column(name = "student_id", nullable = false, length = 255)
+    private String studentId;
+
+    @Column(name = "student_name", nullable = false, length = 255)
+
+    private String studentName;
+
+    @Column(name = "study_program", nullable = false, length = 25)
+    private String studyProgram;
+
+    public Student() {
+    }
+
+    public Student(String studentId, String studentName, String studyProgram) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.studyProgram = studyProgram;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudyProgram() {
+        return studyProgram;
+    }
+
+    public void setStudyProgram(String studyProgram) {
+        this.studyProgram = studyProgram;
+    }
+
+    @Override
+    public String toString() {
+        return studentId + "|" + studentName + "|" + studyProgram;
+    }
+
+}
